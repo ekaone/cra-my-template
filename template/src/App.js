dcdd508aeb24ec5a5f3902ece5a9b9e3b1490c1c
ekaone <ekaone3033@gmail.com>
2022-12-22 14:29:02 +0700

    update

--- template/src/App.js
+++ template/src/App.js
@@ -1,7 +1,25 @@
-import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-const App = () => {
-  return <div>My CRA template</div>;
-};
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Custom Template Eka
+        </a>
+      </header>
+    </div>
+  );
+}
 
 export default App;
